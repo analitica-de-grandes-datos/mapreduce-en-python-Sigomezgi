17d6474505b8fbcc08624134ddebbd7583aa80be
Sigomezgi <94578395+Sigomezgi@users.noreply.github.com>
2022-07-04 23:43:02 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -4,8 +4,6 @@
 import sys
 
 if __name__ == '__main__':
-
-
         for line in sys.stdin:
                 val, key = line.strip().split(",")
                 sys.stdout.write("{},{}\n".format(key, val))
